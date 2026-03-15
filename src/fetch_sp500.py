cc6d02b76bcf72b9d09499a13abbd49bde526527
David David <dcdavid@tutanota.com>
2026-03-15 19:48:40 -0400

    just add a comment

--- src/fetch_sp500.py
+++ src/fetch_sp500.py
@@ -6,7 +6,7 @@ def fetch_sp500(output_filename="../data/sp500_historical_returns.csv"):
     sp500 = yf.download('^GSPC', period='max', interval='1d')
     sp500.columns = sp500.columns.get_level_values(0)
     df = sp500[['Close']].copy()
-    df.dropna(inplace=True)
+    df.dropna(inplace=True) # clean up any days with no price
     df['Log_Return'] = np.log(df['Close'] / df['Close'].shift(1))
     df.dropna(inplace=True) # drop the first row
     df.to_csv(output_filename)
