708839142b09f6528951c630583c547350b6cad0
David David <dcdavid@tutanota.com>
2026-04-07 21:30:12 -0400

    Fixed bug

--- src/agent.py
+++ src/agent.py
@@ -152,7 +152,8 @@ class Agent:
         dynamic_spread = self.spread_pct * current_volatility 
         
         orders = {'agent_id': self.agent_id}
-        orders['bid'] = expected_value * (1 - (0.5 * dynamic_spread)) if self.cash >= bid_price else None
+        bid_price = expected_value * (1 - (0.5 * dynamic_spread))
+        orders['bid'] = bid_price if self.cash >= bid_price else None
         orders['ask'] = expected_value * (1 + (0.5 * dynamic_spread)) if self.shares > 0 else None
 
         return orders if len(orders) > 1 else None
--- src/value_agent.py
+++ src/value_agent.py
@@ -11,7 +11,8 @@ class ValueAgent:
     def generate_bid_ask_spread(self, current_price, current_volatility, current_time, true_value, **kwargs):
         dynamic_spread = self.spread_pct * current_volatility
         orders = {'agent_id': self.agent_id}
-        orders['bid'] = true_value * (1 - (0.5 * dynamic_spread)) if self.cash >= bid_price else None
-        orders['ask'] = true_value * (1 + (0.5 * dynamic_spread)) if self.shares > 0 else None
+        bid_price = expected_value * (1 - (0.5 * dynamic_spread))
+        orders['bid'] = bid_price if self.cash >= bid_price else None
+        orders['ask'] = expected_value * (1 + (0.5 * dynamic_spread)) if self.shares > 0 else None
 
         return orders if len(orders) > 1 else None
