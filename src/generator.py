d061c9b3a628a92b463ec34f57c63198b8b3e4ed
David David <dcdavid@tutanota.com>
2026-04-11 00:13:56 -0400

    Correct volatility drag / geometric drift

--- src/generator.py
+++ src/generator.py
@@ -10,6 +10,7 @@ directly observe V_t. Instead, they only have noisy signals S_{i,t} reflecting
 imperfect information about future payoffs.
 '''
 def generate_fundamental_value(steps=2000, start_price=100, volatility=1.0):
-    shocks = np.random.normal(loc=0, scale=volatility, size=steps)
+    drift_correction = -0.5 * (volatility ** 2) # https://www.kitces.com/blog/volatility-drag-variance-drain-mean-arithmetic-vs-geometric-average-investment-returns/#:~:text=The%20inevitable%20gap%20between%20the,actual%20geometric%20mean%20of%206.94%25.
+    shocks = np.random.normal(loc=drift_correction, scale=volatility, size=steps)
     price_path = start_price * np.exp(np.cumsum(shocks))
     return price_path
